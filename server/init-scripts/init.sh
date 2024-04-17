000e442101de08da322183ae0866a2b75280f101
LoloxDev <loris_labarre@outlook.fr>
2024-04-17 17:58:45 +0200

    Correction init.sh

--- server/init-scripts/init.sh
+++ server/init-scripts/init.sh
@@ -1,10 +1,10 @@
 if [ "$DB_TYPE" == "mysql" ]; then
-    echo "Base de donnée MYSQL détéctée. ✅"
+    echo "Base de données MySQL détectée. ✅"
     mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST $MYSQL_DATABASE < ./db-init/initsql.sql
 elif [ "$DB_TYPE" == "postgres" ]; then
-    echo "Base de donnée POSTGRES détéctée. ✅"
-    psql -U $DB_USER -d $DB_NAME -a -f /docker-entrypoint-initdb.d/db-init/initpg.sql
+    echo "Base de données PostgreSQL détectée. ✅"
+    psql -U $POSTGRES_USER -d $POSTGRES_DB -f docker-entrypoint-initdb.d/db-init/initpg.sql
 else
-    echo "Base de donnée non reconnue! ❌"
+    echo "Base de données non reconnue! ❌"
     exit 1
 fi
