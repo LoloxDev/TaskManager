b48fddb0f42cee65fa7ef263accfcccb592584b1
LoloxDev <loris_labarre@outlook.fr>
2024-06-24 12:29:05 +0200

    test

--- run_eslint.sh
+++ run_eslint.sh
@@ -6,12 +6,14 @@ npm install -D eslint-formatter-junit
 
 OUTPUT_FILE="eslint-result.xml"
 
-# Exécuter ESLint
+# Exécuter ESLint et afficher les erreurs
 npx eslint . -f junit -o $OUTPUT_FILE
 ESLINT_EXIT_CODE=$?
 
 if [ $ESLINT_EXIT_CODE -ne 0 ]; then
     echo "ESLint execution failed with exit code $ESLINT_EXIT_CODE"
+    echo "Linting errors:"
+    npx eslint . --format stylish
     exit $ESLINT_EXIT_CODE
 fi
 
